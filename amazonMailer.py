61d93cfd436c85c0982c1d0e2a33cb4068b7e22e
moioz <33792499+moioz@users.noreply.github.com>
2019-07-22 11:22:15 +0200

    Add files via upload
    added input control and custom mail with product information

--- amazonMailer.py
+++ amazonMailer.py
@@ -1,3 +1,4 @@
+
 # required lib(s)
 import requests
 from os import system
@@ -5,10 +6,10 @@ from bs4 import BeautifulSoup
 import time
 import smtplib
 # set the user agent that the program will use to access amazon (input yours instead of 'bla bla bla')
-headers = {"User-Agent": 'bla bla bla'}
+headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
 # smtp credential
-#insert your data
-smtp_server = 'smtp.google.com'    # google smtp server
+# insert your data
+smtp_server = 'smtp.gmail.com'    # google smtp server
 smtp_port = 587    # google smtp port
 smtp_addrs = ''   # smtp mail
 smtp_passw = ''    # smtp password
@@ -16,6 +17,7 @@ your_email = ''    # reciver mail
 
 # welcome things
 def main():
+    system("cls")
     print("Welcome to AmazonMailer.py")
     print("This program send you an email if the price of an amazon product is less than you want")
     print("Let's go:")
@@ -23,17 +25,31 @@ def main():
 
 def check_price():
     url = input("Input the amazon product url: ")
-    # get the page
-    page = requests.get(url, headers = headers)
-    # read the content of the page
-    soup1 = BeautifulSoup(page.content, "html.parser")
-    soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
-    # search the value of title and price in the page
-    title = soup2.find(id = "productTitle").get_text().strip()
-    price = soup2.find(id = "priceblock_ourprice").get_text().strip()
+    try:
+        # get the page
+        page = requests.get(url, headers = headers)
+        # read the content of the page
+        soup1 = BeautifulSoup(page.content, "html.parser")
+        soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
+        # search the value of title and price in the page
+        title = soup2.find(id = "productTitle").get_text().strip()
+        price = soup2.find(id = "priceblock_ourprice").get_text().strip()
+    except AttributeError:
+        print("It seems that this link is not from amazon.")
+        system("PAUSE")
+        main()
+    except requests.exceptions.MissingSchema:
+        print("It seems that this link is not from amazon.")
+        system("PAUSE")
+        main()
+
     # converting the string into a float
-    price=float(price[0:-5])
-    print("The product '", title, "' costs ", price)
+    try:
+        price=float(price[0:-5])
+        print("The product '", title, "' costs ", price)
+    except ValueError:
+        print("Product with wrong price layout!")
+        main()
     # confirm
     save = input("Do you want to track the cost of this (y/n): ")
     if save == 'y' or save == 'Y':
@@ -42,20 +58,21 @@ def check_price():
         your_price = input("Type the price that you want: ")
         while True:
             if price < float(your_price):
-                send_email()
+                send_email(title, url)
                 print("EMAIL SENDED")
                 break
                 system("EXIT")
             else:
+                print("MONITORING AMAZON...")
                 time.sleep(3600)
                 continue
     else:
-        print("Back to main menu")
+        print("Your choice is wrong. Back to main menu")
         system("PAUSE")
-        system("cls")
         main()
+    return url, title, price
 
-def send_email():
+def send_email(title, url):
     server = smtplib.SMTP(smtp_server, smtp_port)
 
     #logging to server
@@ -64,8 +81,8 @@ def send_email():
     server.ehlo()
     server.login(smtp_addrs, smtp_passw)
 
-    subject = "The product that you are tracking has reached your price!"
-    body = ("Go to the app and SHOPPING!")
+    subject = "The product '{}' that you are tracking has reached your price!".format(title)
+    body = "Hi. \nThis is an automatic mail sended you by AmazonMailer. \nThe product that you war tracking as reached your price. check the link \n{}\nand BUY IT!".format(url)
     message = f"Subject: {subject}\n\n{body}"
 
     # sent the email
